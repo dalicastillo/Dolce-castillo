9d2c6be217a049eab0d6b9c8909a00697f171482
dalicastillo <88114691+dalicastillo@users.noreply.github.com>
2021-09-20 17:30:28 -0300

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import * as ReactBootStrap from "react-bootstrap";
+import './NavBar.css' ;
+
+function NavBar() {
+    return (
+        
+          <div className='menu'>
+           <>
+    <ReactBootStrap.Navbar >
+    <ReactBootStrap.Container>
+    <ReactBootStrap.Navbar.Brand href="#home">  <img
+          alt=""
+          src="https://image.flaticon.com/icons/png/512/4139/4139679.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      Dolce Tempo</ReactBootStrap.Navbar.Brand>
+    <ReactBootStrap.Nav className="me-auto">
+      <ReactBootStrap.Nav.Link href="#home">Inicio</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#features">Productos</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#pricing">Ofertas</ReactBootStrap.Nav.Link>
+      <svg xmlns="http://www.w3.org/2000/svg" width="1700" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+        </svg>
+    </ReactBootStrap.Nav>
+    </ReactBootStrap.Container>
+  </ReactBootStrap.Navbar>
+
+        
+       
+   
+</>
+           </div>
+        
+    );
+}
+
+
+
+export default NavBar;
---
+++ src/components/cards.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import * as ReactBootStrap from "react-bootstrap";
+import Contador from './Contados'
+
+
+
+
+function Cards() {
+    return (
+  <div style={{display: "flex",justifyContent:"space-evenly"}} className='cards'> 
+
+    <ReactBootStrap.Card border="secondary" style={{ width: '18rem' }}>
+      <ReactBootStrap.Card.Img variant="top" src="https://scontent.feze13-1.fna.fbcdn.net/v/t1.6435-9/241956365_1464058273965633_4097221478472715677_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=a26aad&_nc_ohc=2DUjTCp8fL8AX9YvtLv&_nc_ht=scontent.feze13-1.fna&oh=6d759bbc5016520677275b323c6813ec&oe=61672F87" />
+       <ReactBootStrap.Card.Body>
+    <ReactBootStrap.Card.Title>Torta y galletas</ReactBootStrap.Card.Title>
+       <ReactBootStrap.Card.Text>
+      Incluye torta, galletas y cupcake personalizado con sabores a elección
+    </ReactBootStrap.Card.Text>
+     <ReactBootStrap.Button variant="info">Consultar</ReactBootStrap.Button>
+       </ReactBootStrap.Card.Body>
+       <h2>$1400</h2>
+      <Contador />
+    <ReactBootStrap.Card.Footer>
+      <small className="text-muted">Disponible en stock</small>
+    </ReactBootStrap.Card.Footer>
+    </ReactBootStrap.Card>
+    <ReactBootStrap.Card border="secondary" style={{ width: '18rem' }}>
+       <ReactBootStrap.Card.Img variant="top" src="https://scontent.feze13-1.fna.fbcdn.net/v/t1.6435-9/241279034_1460809327623861_9020599761311698941_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=a26aad&_nc_ohc=fpwRrKjR74QAX_GXx3I&_nc_ht=scontent.feze13-1.fna&oh=4153be8d8fd43b2144ccc08b89a52e2c&oe=6168B717" />
+       <ReactBootStrap.Card.Body>
+    <ReactBootStrap.Card.Title>Torta desayuno</ReactBootStrap.Card.Title>
+       <ReactBootStrap.Card.Text>
+       Incluye torta y taza personalizadas + sandwiches, medialunas de JyQ, icepop, conos de ddl y alfajores de maicena
+    </ReactBootStrap.Card.Text>
+     <ReactBootStrap.Button variant="info">Consultar</ReactBootStrap.Button>
+       </ReactBootStrap.Card.Body>
+       <h2>$1200</h2>
+    <Contador/>
+    <ReactBootStrap.Card.Footer>
+      <small className="text-muted">Disponible en stock</small>
+    </ReactBootStrap.Card.Footer>
+    </ReactBootStrap.Card>
+    <ReactBootStrap.Card border="secondary" style={{ width: '18rem' }}>
+       <ReactBootStrap.Card.Img variant="top" src="https://scontent.feze13-1.fna.fbcdn.net/v/t1.6435-9/241520436_1459713754400085_6626014556035934234_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=a26aad&_nc_ohc=V81urXA10PMAX9RutQ9&_nc_ht=scontent.feze13-1.fna&oh=c86aaaa8b982dff10faba81557e7cdad&oe=61685F1A" />
+       <ReactBootStrap.Card.Body>
+    <ReactBootStrap.Card.Title>Box dulce</ReactBootStrap.Card.Title>
+       <ReactBootStrap.Card.Text>
+     Incluye icepop, bombones de chocolate, galletas, oreos bañadas en chocolate y mini tabletas de chocolate. Tematica a elección
+    </ReactBootStrap.Card.Text>
+     <ReactBootStrap.Button variant="info">Consultar</ReactBootStrap.Button>
+       </ReactBootStrap.Card.Body>
+       <h2>$800</h2>
+      <Contador />
+    <ReactBootStrap.Card.Footer>
+      <small className="text-muted">Consultar disponibilidad de stock</small>
+    </ReactBootStrap.Card.Footer>
+    </ReactBootStrap.Card>
+
+
+    </div>
+        
+        );
+    }
+
+
+export default Cards;
